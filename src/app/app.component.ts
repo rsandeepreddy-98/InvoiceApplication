127ee120a5a17e8891de7c3db128efed3b3df768
sandeep <sandeep.com>
2021-09-19 20:12:05 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'InvoiceApplication';
+  title = 'InvoiceApplication'; 
+  loginform=new FormGroup({
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(6)]),
+    gender:new FormControl()
+    
+  })
+    // loginuser(){
+    //   console.log(this.loginform.value);
+    // }
+  
+  get email(){
+    return this.loginform.get('email')
+  }
+
+  get password(){
+    return this.loginform.get('password')
+  }
+  get gender(){
+    return this.loginform.get('gender');
+  }
+
+  
+   
+  submit(){
+    alert('Details submitted')
+   console.log(this.loginform.value);
+  } 
+  onreset(){
+    this.loginform.reset;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule,FormsModule, NgModel } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
